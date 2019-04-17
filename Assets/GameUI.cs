d04481bf0ebaca2fdc9902940ef9a58fde393571
Boud <maxime.boudreault.0@gmail.com>
2019-04-16 21:52:48 -0400

    Create game ui an link gameobjects

---
+++ Assets/GameUI.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject JumpScare;
+    public GameObject[] Lives;
+}
