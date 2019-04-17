a567998f8099c68ea8605c98e14d178d5d680aaa
Boud <maxime.boudreault.0@gmail.com>
2019-04-16 21:15:24 -0400

    Prepare Gille le monstre

---
+++ Assets/GilleLeMonstre.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GilleLeMonstre : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 1.33f;
+
+    void FixedUpdate()
+    {
+        ChasePlayer();
+    }
+
+    void ChasePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.LookAt(player);
+    }
+}
